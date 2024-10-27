1ef70b0a6bdbf6d62504b8c257d69c4ee53e26d3
JCCRUZCHACON <confeccionesmc2018@gmail.com>
2024-10-27 16:28:27 -0500

    advances

---
+++ src/services/hotel.services.js
@@ -0,0 +1,30 @@
+const { hotel, city, image} = require('../models');
+
+const getAllServices = async () => {
+  return await hotel.findAll({ include: [city, image] });
+   
+};
+
+const createServices = async (body) => {
+  return await hotel.create(body)
+}
+
+const getOneServices = async (id) => {
+  return await hotel.findByPk(id, { include: [city, image] })
+}
+
+const updateServices = async (body, id) => {
+  return await hotel.update(body, { where: { id }, returning: true });
+};
+
+const removeServices = async (id) => {
+  return await hotel.destroy({ where: { id } });
+};
+
+module.exports = {
+  getAllServices,
+  createServices,
+  getOneServices,
+  updateServices,
+  removeServices
+}
