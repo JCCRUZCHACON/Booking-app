d8da91168b4758633c0558b571e557b34010569f
JCCRUZCHACON <confeccionesmc2018@gmail.com>
2025-05-13 16:44:23 -0500

    advance

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,15 @@
-require('pg')
-require('pg-hstore')
+require('pg');
+require('pg-hstore');
 require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL'
+    use_env_variable: 'DATABASE_URL_DEV',
+    dialect: 'postgres',
   },
   test: {
-    use_env_variable: 'DATABASE_URL'
+    use_env_variable: 'DATABASE_URL_TEST',
+    dialect: 'postgres',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
@@ -15,8 +17,8 @@ module.exports = {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-}
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,31 +1,34 @@
 const app = require('./app');
-// const { exec } = require('child_process');
+
+// Para ejecutar migraciones automáticamente en producción:
+//const { exec } = require('child_process');
 
 const PORT = process.env.PORT || 3000;
 
 const main = async () => {
   try {
-    // Cambié la configuración de listen para escuchar en todas las interfaces
+    // Escucha en todas las interfaces si estamos en producción
     app.listen(PORT, '::', () => {
       console.log(`👉 Server running on port ${PORT}`);
       console.log(`👉 Link http://localhost:${PORT}`);
     });
-    
-    // Solo descomentar esta parte cuando sea necesario ejecutar migraciones
-    // await new Promise((resolve, reject) => {
-    //   const migrate = exec(
-    //     'sequelize db:migrate',
-    //     { env: process.env },
-    //     err => (err ? reject(err) : resolve())
-    //   );
 
-    //   // Forward stdout+stderr to this process
-    //   migrate.stdout.pipe(process.stdout);
-    //   migrate.stderr.pipe(process.stderr);
-    // });
+    // Solo ejecuta las migraciones en desarrollo (no en producción)
+    // if (process.env.NODE_ENV === 'development') {
+    //   console.log('🛠️ En modo desarrollo, ejecutando migraciones...');
+    //   await new Promise((resolve, reject) => {
+    //     const migrate = exec(
+    //       'sequelize db:migrate', 
+    //       { env: process.env }, 
+    //       err => (err ? reject(err) : resolve())
+    //     );
 
+    //     migrate.stdout.pipe(process.stdout); // Muestra salida estándar
+    //     migrate.stderr.pipe(process.stderr); // Muestra errores
+    //   });
+    // }
   } catch (error) {
-    console.log(error);
+    console.log('🚨 Error al iniciar el servidor:', error);
   }
 }
 
